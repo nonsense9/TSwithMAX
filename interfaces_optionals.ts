a72aaacf874c08f4e49ed6a7a385fdbbc7734f98
nonsense9 <67061900+nonsense9@users.noreply.github.com>
2020-09-10 10:44:41 +0300

    interfaces_and_optional_operators

---
+++ interfaces_optionals.ts
@@ -0,0 +1,43 @@
+// interface AddFn {
+//   (a: number, b: number): number;
+// }
+
+// let add1: AddFn;
+
+// add1 = (n1: number, n2: number) => {
+//   return n1 + n2;
+// };
+
+// interface Named {
+//   readonly name?: string;
+//   outputName?: string;
+// }
+
+// interface Greetable extends Named {
+//   greet(phrase: string): void;
+// }
+
+// class Person implements Greetable {
+//   name?: string;
+//   age = 27;
+
+//   constructor (n?: string) {
+//     if (n) {
+//     this.name = n;
+//     }
+//   }
+//   greet(phrase: string) {
+//     if (this.name) {
+//       console.log(phrase + ' ' + this.name);
+//     } else {
+//       console.log('Hi');      
+//     }
+// }
+// }
+
+// let user1: Greetable;
+
+// user1 = new Person('Alex');
+
+// user1.greet('Hi there - I am');
+// console.log(user1);
